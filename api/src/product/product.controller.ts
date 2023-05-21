efc790872696d794997ed7b596ebb5df876659c9
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-05-21 19:25:34 +0530

    feat: Product module and routes

---
+++ api/src/product/product.controller.ts
@@ -0,0 +1,27 @@
+import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {ProductService} from "./product.service";
+import {ProductDocument} from "./product.schema";
+
+@Controller('product')
+export class ProductController {
+    constructor(private readonly productService: ProductService) {
+    }
+
+    @Post()
+    createProduct(
+        @Body('name') name: string,
+        @Body('price') price: number,
+        @Body('description') description?: string,
+    ):Promise<ProductDocument> {
+        return this.productService.create(name, price, description);
+    }
+
+    @Get()
+    findAllProducts():Promise<ProductDocument[]> {
+        return this.productService.findAll();
+    }
+    @Get(':id')
+    findProduct(@Param('id') id: string):Promise<ProductDocument> {
+        return this.productService.find(id);
+    }
+}
