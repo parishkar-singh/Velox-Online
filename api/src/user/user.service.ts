aedd1344d1a166dbf36e6709788e76e64f1b015a
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-05-22 00:14:11 +0530

    feat: user DTO's and export enabled

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
-
+import {Injectable} from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {Model} from "mongoose";
+import {User, UserDocument} from "./user.schema";
+import {UserDetails} from "./user-details.interface";
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectModel('User') private readonly userModel: Model<UserDocument>) {
+    }
+    _getUserDetails(user: UserDocument): UserDetails {
+        return {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+        }
+    }
+    async create(name: string, email: string, hashedPassword: string): Promise<UserDocument> {
+        const newUser = new this.userModel({
+            name,
+            email,
+            password: hashedPassword
+        });
+        return await newUser.save();
+    }
+    async find(id: string): Promise<UserDocument> {
+        return await this.userModel.findById(id).exec();
+    }
+}
