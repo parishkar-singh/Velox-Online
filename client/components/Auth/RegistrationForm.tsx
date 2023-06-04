14ce3d2535e54012e8c3a191ea7399a5ca8a3896
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-06-04 16:03:17 +0530

    feat: email and validator password

--- client/components/Auth/RegistrationForm.tsx
+++ client/components/Auth/RegistrationForm.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import {Box, Grid, TextField, InputLabel, Typography, Button, Divider} from "@mui/material";
 import Link from "next/link";
 import useInput from "@/hooks/input/use-input";
-import {validateNameLength} from "@/utils/validation/length";
+import {validateNameLength, ValidatePasswordLength} from "@/utils/validation/length";
+import {ValidateEmail} from "@/utils/validation/email";
+
 const RegistrationForm: React.FC = () => {
     const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -15,18 +17,56 @@ const RegistrationForm: React.FC = () => {
         inputBlurHandler: nameBlurHandler,
         clearHandler: nameClearHandler,
     } = useInput(validateNameLength);
+    const {
+        text: email,
+        shouldDisplayError: emailHasError,
+        textChangeHandler: emailChangeHandler,
+        inputBlurHandler: emailBlurHandler,
+        clearHandler: emailClearHandler,
+    } = useInput(ValidateEmail);
+    const {
+        text: password,
+        shouldDisplayError: passwordHasError,
+        textChangeHandler: passwordChangeHandler,
+        inputBlurHandler: passwordBlurHandler,
+        clearHandler: passwordClearHandler,
+    } = useInput(ValidatePasswordLength);
+    const {
+        text: confirmPassword,
+        shouldDisplayError: confirmPasswordHasError,
+        textChangeHandler: confirmPasswordChangeHandler,
+        inputBlurHandler: confirmPasswordBlurHandler,
+        clearHandler: confirmPasswordClearHandler,
+    } = useInput(ValidatePasswordLength);
+    
+
+
     return (
-        <div className={`bg-white rounded-3xl border-2 border-neutral-200 p-4`}>
-            <Box sx={{ width: '350px', marginTop: 2}}>
+        <div className={`bg-white mt-2 border-2 border-neutral-200 p-4`}>
+            <Box sx={{width: '350px', marginTop: 2}}>
                 <form onSubmit={onSubmitHandler}>
                     <Grid container className={`flex flex-col justify-items-start p-2`}>
                         <span className={`text-4xl mb-4`}>Create Account</span>
                         <InputLabel className={`text-medium mt-1`} htmlFor={'name'}>Your Name</InputLabel>
-                        <TextField value={name} onChange={nameChangeHandler} onBlur={nameBlurHandler} error={nameHasError} helperText={nameHasError?'Enter your Name':' '} type={'text'} name={'name'} id={`name`} variant={`outlined`} size={'small'}/>
+                        <TextField value={name} onChange={nameChangeHandler} onBlur={nameBlurHandler}
+                                   error={nameHasError} helperText={nameHasError ? 'Enter your Name' : ' '}
+                                   type={'text'} name={'name'} id={`name`} variant={`outlined`} size={'small'}/>
                         <InputLabel className={`text-medium mt-1`} htmlFor={'email'}>Email</InputLabel>
-                        <TextField type={'text'} name={'email'} id={`email`} variant={`outlined`} size={'small'}/>
+                        <TextField
+                            value={email}
+                            onChange={emailChangeHandler}
+                            onBlur={emailBlurHandler}
+                            error={emailHasError}
+                            helperText={emailHasError ? 'Enter a valid email' : ' '}
+                            type={'text'} name={'email'} id={`email`} variant={`outlined`} size={'small'}/>
                         <InputLabel className={`text-medium mt-1`} htmlFor={'password'}>Password</InputLabel>
-                        <TextField type={'text'} name={'password'} id={`password`} variant={`outlined`} size={'small'}
+                        <TextField
+                            value={password}
+                            onChange={passwordChangeHandler}
+                            onBlur={passwordBlurHandler}
+                            error={passwordHasError}
+                            helperText={passwordHasError ? 'Enter a valid password' : ' '}
+                            type={'password'} name={'password'} id={`password`} variant={`outlined`} size={'small'}
                                    placeholder={`Minimum 6 characters required`}/>
                         <InputLabel className={`text-medium mt-1`} htmlFor={'confirmPassword'}>Re-Enter
                             Password</InputLabel>
@@ -42,7 +82,8 @@ const RegistrationForm: React.FC = () => {
                 </div>
                 <div className={`mt-[10px]`}>
                     <small>
-                        <a className={`text-blue-700`} href={`#`}>Conditions of use</a>{``} and {``} <a className={`text-blue-700`} href={`#`}>Privacy Notice</a>
+                        <a className={`text-blue-700`} href={`#`}>Conditions of use</a>{``} and {``} <a
+                        className={`text-blue-700`} href={`#`}>Privacy Notice</a>
                     </small>
                 </div>
                 <Divider className={`mt-2`}/>
